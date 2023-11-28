be06ad8e70c4282abdfc5b1e1e91c4b3f603125e
roope heinonen <roope.heinonen@helsinki.fi>
2023-11-28 16:09:19 +0200

    added flag which enables support for null costs in the customCostFactors HashMap, added to tests

--- src/test/java/com/conveyal/r5/customcost/CustomCostTest.java
+++ src/test/java/com/conveyal/r5/customcost/CustomCostTest.java
@@ -6,9 +6,13 @@ import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,13 +37,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class CustomCostTest {
     private static final Logger LOG = LoggerFactory.getLogger(ReverseRoutingTest.class);
 
-    HashMap<Long, Double> customCostHashMap;
+    HashMap<Long, Double> staticCustomCostHashMap;
 
     @BeforeEach
     public void setUp () throws Exception {
-        customCostHashMap = generateCustomCostHashMap();
+        staticCustomCostHashMap = generateCustomCostHashMap();
     }
 
+    // this is used in places where no customised cost map is needed
     public static HashMap<Long, Double> generateCustomCostHashMap() {
         HashMap<Long, Double> osmIdMap = new HashMap<>();
         // put a known value as the first in the hashmap
@@ -57,26 +62,27 @@ public class CustomCostTest {
 
     @Test
     public void testCreateValidCustomCostField () {
-        CustomCostField customCostInstance = new CustomCostField("testKey", 3, customCostHashMap);
-        double targetValue = customCostHashMap.get(123123L);
+        CustomCostField customCostInstance = new CustomCostField("testKey", 3, staticCustomCostHashMap, false);
+        double targetValue = staticCustomCostHashMap.get(123123L);
         assertEquals(customCostInstance.getDisplayKey(), "testKey");
         assertEquals(targetValue, 7.0);
-        assertEquals(customCostHashMap.size() > 0, true);
+        assertEquals(staticCustomCostHashMap.size() > 0, true);
     }
 
     @Test
     public void testCreateInvalidCustomCostField () {
         HashMap<Long, Double> emptyHashmap = new HashMap<>();
         // assertThrows(IllegalArgumentException.class, );
-        assertThrows(IllegalArgumentException.class, () -> new CustomCostField("testKey", 3, emptyHashmap));
-        assertThrows(IllegalArgumentException.class, () -> new CustomCostField("testKey", 3, null));
+        assertThrows(IllegalArgumentException.class, () -> new CustomCostField("testKey", 3, emptyHashmap, false));
+        assertThrows(IllegalArgumentException.class, () -> new CustomCostField("testKey", 3, null, false));
     }
 
     /**
      * Got the grid layout and the test from SimpsonDesertTests.java
      */
-    @Test
-    public void testRoutingWithCustomCosts() { 
+    @ParameterizedTest
+    @MethodSource("customCostTestParameterProvider")
+    public void testRoutingWithCustomCosts(boolean allowNullCustomCostEdges) { 
         // this test could be made more robust by:
         // 1. use different methods of creating the network and the task
         // which would let us see that the times have increased the amount we set
@@ -87,19 +93,11 @@ public class CustomCostTest {
 
         GridLayout gridLayout = new GridLayout(SIMPSON_DESERT_CORNER, 6);
         TransportNetwork Network = gridLayout.generateNetwork();
-        // take all the osmIds that the network has
+  
+        // take osmIds that the network has
+        // if allowNullCustomCostEdges is true, take only 1/2 of osids for testing missing osmids
         // add them to a list and make sure that they exist
-        // notice: for some reason this logic doesn't find all the osmIds
-        List<Long> osmIds = new ArrayList<>();
-        EdgeStore.Edge e = Network.streetLayer.edgeStore.getCursor(0);
-        do {
-            osmIds.add(e.getOSMID());
-        } while (e.advance());
-
-        final List<Long> uniqueOsmIds = osmIds.stream().distinct().collect(Collectors.toList());
-
-        assertTrue(uniqueOsmIds.size() > 0);
-
+        List<Long> uniqueOsmIds = getOsmIds(Network, allowNullCustomCostEdges);
         HashMap<Long, Double> customCostHashMap = new HashMap<>();
         // add the acquired osmIds to a hashmap with random values
         for (Long osmId : uniqueOsmIds) {
@@ -109,7 +107,7 @@ public class CustomCostTest {
         }
 
         // add the hashmap as the customCostHashMap to the customCostInstance
-        CustomCostField customCostInstance = new CustomCostField("testKey", 2, customCostHashMap);
+        CustomCostField customCostInstance = new CustomCostField("testKey", 2, customCostHashMap, allowNullCustomCostEdges);
         Network.streetLayer.edgeStore.costFields = CustomCostField.wrapToEdgeStoreCostFieldsList(customCostInstance);
 
         // build the task from the grid, example taken from SimpsonDesertTests.java
@@ -127,11 +125,20 @@ public class CustomCostTest {
         assertTrue(oneOriginResult.osmIdResults.size() > 0);
         assertTrue(oneOriginResult.travelTimes.nPoints > 0);
 
-        // test that the routing result osmId has only valid osmIds which are found from the network
-        // this is important, that atleast some values have the same osmids -> they will be added custom costs
-        boolean hasOnlyValidOsmIdsFoundFromNetwork = oneOriginResult.osmIdResults.stream()
-            .allMatch(innerList -> innerList.stream().allMatch(uniqueOsmIds::contains));
-        assertTrue(hasOnlyValidOsmIdsFoundFromNetwork);
+        // test that the routing result osmId has valid osmIds which are found from the network
+        // test for some valid if using partial osmids, test for allMatch if using all osmids
+        if(allowNullCustomCostEdges) {
+            // Check if any OSM ID from the results is in the subset
+            boolean hasSomeValidOsmIdsFoundFromNetwork = oneOriginResult.osmIdResults.stream()
+                .flatMap(List::stream)
+                .anyMatch(uniqueOsmIds::contains);
+            assert(hasSomeValidOsmIdsFoundFromNetwork);
+        }
+        else {
+            boolean hasOnlyValidOsmIdsFoundFromNetwork = oneOriginResult.osmIdResults.stream()
+                .allMatch(innerList -> innerList.stream().allMatch(uniqueOsmIds::contains));
+            assertTrue(hasOnlyValidOsmIdsFoundFromNetwork);
+        }
 
         int [][] travelTimeValues = oneOriginResult.travelTimes.getValues();
 
@@ -171,13 +178,94 @@ public class CustomCostTest {
 
         // check that customCost value is always same or bigger, never smaller
         assertTrue(allValuesIncreasedOrSame);
+<<<<<<< Updated upstream
     }
 
      @Test
     public void testInvalidCustomCostMapOsmIdNotFound() { 
+=======
+
+        // test that the custom cost values are correctly applied
+        // and that the base time and custom cost "time" maps have correct values
+
+        assertTrue(customCostFieldsList.size() > 0);
+
+        // check that all the customCostFields have non-empty baseTraveltimesMap
+        // and uniqueOsmIds size is the same and all the osmIds are found from the map
+        // for they are were traversed so they should be present
+        for (CustomCostField customCostField : customCostFieldsList) {
+            HashMap<Long, Integer> baseTraveltimesMap = customCostField.getBaseTraveltimes();
+            assertTrue(baseTraveltimesMap.size() > 0);
+            if(!allowNullCustomCostEdges) {
+                assertTrue(baseTraveltimesMap.size() == uniqueOsmIds.size());
+                assertTrue(baseTraveltimesMap.keySet().stream().allMatch(uniqueOsmIds::contains));
+            }
+            else {
+                assertTrue(baseTraveltimesMap.size() > uniqueOsmIds.size());
+                assertTrue(baseTraveltimesMap.keySet().stream().anyMatch(uniqueOsmIds::contains));
+            }
+        }
+
+        // check that the maps have correct values and that we get same values from the map and the manual calculation
+        for (CustomCostField customCostField : customCostFieldsList) {
+            HashMap<Long, Integer> baseTraveltimesMap = customCostField.getBaseTraveltimes();
+            HashMap<Long, Integer> customCostAdditionalTravelTimesMap = customCostField.getcustomCostAdditionalTraveltimes();
+            for (Long osmId : uniqueOsmIds) {
+                // calculate cost manually
+                int baseTraveltime = baseTraveltimesMap.get(osmId);
+                double customCostFactor = customCostHashMap.get(osmId);
+                double sensitivity = customCostField.getSensitivityCoefficient();
+                // custom cost additional seconds added to base travel time
+                int additionalCostSeconds = (int) Math.round(baseTraveltime * customCostFactor * sensitivity);
+                // final travel time with custom cost
+                int finalManuallyCalculatedCost = baseTraveltime + additionalCostSeconds;
+                // use maps to get the final travel time with custom cost using osmId as key
+                int customCostAdditionalTraveltimeCost = customCostAdditionalTravelTimesMap.get(osmId);
+                // final travel time with custom cost from maps
+                int customCostFinalTraveltimeCostFromMaps = baseTraveltime + customCostAdditionalTraveltimeCost;
+                // see that manually calculated value match with maps values
+                assertEquals(finalManuallyCalculatedCost, customCostFinalTraveltimeCostFromMaps);
+            }
+        }
+    }
+
+    // use parameterization for testing allowNullCustomCostEdges flag
+    // for all osmids and just some osmids
+    static Stream<Arguments> customCostTestParameterProvider() {
+        return Stream.of(
+            // run with for all osmIds
+            Arguments.of(false),
+            // run with some osmIds
+            Arguments.of(true)
+        );
+    }
+
+    public List<Long> getOsmIds(TransportNetwork network, boolean getOnlyPartialOsmidList) {
+        List<Long> osmIds = new ArrayList<>();
+        EdgeStore.Edge e = network.streetLayer.edgeStore.getCursor(0);
+        do {
+            osmIds.add(e.getOSMID());
+        } while (e.advance());
+
+        List<Long> uniqueOsmIds = osmIds.stream().distinct().collect(Collectors.toList());
+
+        assertTrue(uniqueOsmIds.size() > 0);
+
+        // get only 1/3 of all osmids
+        // used to test flag allowNullCustomCostEdges
+        if (getOnlyPartialOsmidList) {
+            uniqueOsmIds = uniqueOsmIds.subList(0, uniqueOsmIds.size() / 2);
+        }
+      
+        return uniqueOsmIds;
+    }
+
+    @Test
+    public void testInvalidCustomCostFactorsOsmIdNotFound() { 
+>>>>>>> Stashed changes
         GridLayout gridLayout = new GridLayout(SIMPSON_DESERT_CORNER, 6);
         TransportNetwork Network = gridLayout.generateNetwork();
-        CustomCostField customCostInstance = new CustomCostField("testKey", 1.5, customCostHashMap);
+        CustomCostField customCostInstance = new CustomCostField("testKey", 1.5, staticCustomCostHashMap, false);
         Network.streetLayer.edgeStore.costFields = CustomCostField.wrapToEdgeStoreCostFieldsList(customCostInstance);
 
         // build the task from the grid, example taken from SimpsonDesertTests.java
